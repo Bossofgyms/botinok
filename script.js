926545d6136b761da0d671582957cb5488f92853
Bossofgyms <wower-01@mail.ru>
2025-10-06 07:21:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,484 +1,3 @@
-// Полная колода Таро - 78 карт
-const TAROT_DECK = [
-    // СТАРШИЕ АРКАНЫ (22 карты)
-    { 
-        name: "0. Шут", 
-        meaning: "Начало, невинность, спонтанность", 
-        image: "images/fool.jpg",
-        type: "major"
-    },
-    { 
-        name: "I. Маг", 
-        meaning: "Воля, мастерство, концентрация", 
-        image: "images/magician.jpg",
-        type: "major"
-    },
-    { 
-        name: "II. Верховная Жрица", 
-        meaning: "Интуиция, тайны, мудрость", 
-        image: "images/high_priestess.jpg",
-        type: "major"
-    },
-    { 
-        name: "III. Императрица", 
-        meaning: "Изобилие, творчество, природа", 
-        image: "images/empress.jpg",
-        type: "major"
-    },
-    { 
-        name: "IV. Император", 
-        meaning: "Власть, структура, контроль", 
-        image: "images/emperor.jpg",
-        type: "major"
-    },
-    { 
-        name: "V. Иерофант", 
-        meaning: "Традиция, вера, духовность", 
-        image: "images/hierophant.jpg",
-        type: "major"
-    },
-    { 
-        name: "VI. Влюбленные", 
-        meaning: "Выбор, отношения, гармония", 
-        image: "images/lovers.jpg",
-        type: "major"
-    },
-    { 
-        name: "VII. Колесница", 
-        meaning: "Движение, победа, контроль", 
-        image: "images/chariot.jpg",
-        type: "major"
-    },
-    { 
-        name: "VIII. Сила", 
-        meaning: "Мужество, страсть, влияние", 
-        image: "images/strength.jpg",
-        type: "major"
-    },
-    { 
-        name: "IX. Отшельник", 
-        meaning: "Самоанализ, уединение, мудрость", 
-        image: "images/hermit.jpg",
-        type: "major"
-    },
-    { 
-        name: "X. Колесо Фортуны", 
-        meaning: "Судьба, циклы, удача", 
-        image: "images/wheel.jpg",
-        type: "major"
-    },
-    { 
-        name: "XI. Справедливость", 
-        meaning: "Баланс, правда, карма", 
-        image: "images/justice.jpg",
-        type: "major"
-    },
-    { 
-        name: "XII. Повешенный", 
-        meaning: "Жертва, сдача, новая перспектива", 
-        image: "images/hanged.jpg",
-        type: "major"
-    },
-    { 
-        name: "XIII. Смерть", 
-        meaning: "Конец, трансформация, переход", 
-        image: "images/death.jpg",
-        type: "major"
-    },
-    { 
-        name: "XIV. Умеренность", 
-        meaning: "Баланс, терпение, гармония", 
-        image: "images/temperance.jpg",
-        type: "major"
-    },
-    { 
-        name: "XV. Дьявол", 
-        meaning: "Искушение, зависимость, материализм", 
-        image: "images/devil.jpg",
-        type: "major"
-    },
-    { 
-        name: "XVI. Башня", 
-        meaning: "Внезапные изменения, откровение", 
-        image: "images/tower.jpg",
-        type: "major"
-    },
-    { 
-        name: "XVII. Звезда", 
-        meaning: "Надежда, вдохновение, духовность", 
-        image: "images/star.jpg",
-        type: "major"
-    },
-    { 
-        name: "XVIII. Луна", 
-        meaning: "Иллюзия, страх, подсознание", 
-        image: "images/moon.jpg",
-        type: "major"
-    },
-    { 
-        name: "XIX. Солнце", 
-        meaning: "Радость, успех, жизненная сила", 
-        image: "images/sun.jpg",
-        type: "major"
-    },
-    { 
-        name: "XX. Суд", 
-        meaning: "Возрождение, призыв, прощение", 
-        image: "images/judgement.jpg",
-        type: "major"
-    },
-    { 
-        name: "XXI. Мир", 
-        meaning: "Завершение, единство, достижение", 
-        image: "images/world.jpg",
-        type: "major"
-    },
-
-    // МЛАДШИЕ АРКАНЫ - ЖЕЗЛЫ (14 карт)
-    { 
-        name: "Туз Жезлов", 
-        meaning: "Вдохновение, энергия, потенциал", 
-        image: "images/wands01.jpg",
-        type: "wands"
-    },
-    { 
-        name: "2 Жезлов", 
-        meaning: "Планирование, решение, партнерство", 
-        image: "images/wands02.jpg",
-        type: "wands"
-    },
-    { 
-        name: "3 Жезлов", 
-        meaning: "Предвидение, сотрудничество, расширение", 
-        image: "images/wands03.jpg",
-        type: "wands"
-    },
-    { 
-        name: "4 Жезлов", 
-        meaning: "Стабильность, праздник, основание", 
-        image: "images/wands04.jpg",
-        type: "wands"
-    },
-    { 
-        name: "5 Жезлов", 
-        meaning: "Конфликт, конкуренция, дебаты", 
-        image: "images/wands05.jpg",
-        type: "wands"
-    },
-    { 
-        name: "6 Жезлов", 
-        meaning: "Победа, успех, признание", 
-        image: "images/wands06.jpg",
-        type: "wands"
-    },
-    { 
-        name: "7 Жезлов", 
-        meaning: "Стойкость, вызов, защита", 
-        image: "images/wands07.jpg",
-        type: "wands"
-    },
-    { 
-        name: "8 Жезлов", 
-        meaning: "Скорость, действие, движение", 
-        image: "images/wands08.jpg",
-        type: "wands"
-    },
-    { 
-        name: "9 Жезлов", 
-        meaning: "Сила, стойкость, бдительность", 
-        image: "images/wands09.jpg",
-        type: "wands"
-    },
-    { 
-        name: "10 Жезлов", 
-        meaning: "Бремя, ответственность, давление", 
-        image: "images/wands10.jpg",
-        type: "wands"
-    },
-    { 
-        name: "Паж Жезлов", 
-        meaning: "Энтузиазм, исследование, новости", 
-        image: "images/wands11.jpg",
-        type: "wands"
-    },
-    { 
-        name: "Рыцарь Жезлов", 
-        meaning: "Приключение, энергия, импульс", 
-        image: "images/wands12.jpg",
-        type: "wands"
-    },
-    { 
-        name: "Королева Жезлов", 
-        meaning: "Уверенность, независимость, харизма", 
-        image: "images/wands13.jpg",
-        type: "wands"
-    },
-    { 
-        name: "Король Жезлов", 
-        meaning: "Лидерство, предпринимательство, видение", 
-        image: "images/wands14.jpg",
-        type: "wands"
-    },
-
-    // МЛАДШИЕ АРКАНЫ - КУБКИ (14 карт)
-    { 
-        name: "Туз Кубков", 
-        meaning: "Новая любовь, эмоциональное начало", 
-        image: "images/cups01.jpg",
-        type: "cups"
-    },
-    { 
-        name: "2 Кубков", 
-        meaning: "Партнерство, союз, притяжение", 
-        image: "images/cups02.jpg",
-        type: "cups"
-    },
-    { 
-        name: "3 Кубков", 
-        meaning: "Праздник, дружба, сообщество", 
-        image: "images/cups03.jpg",
-        type: "cups"
-    },
-    { 
-        name: "4 Кубков", 
-        meaning: "Апатия, созерцание, отступление", 
-        image: "images/cups04.jpg",
-        type: "cups"
-    },
-    { 
-        name: "5 Кубков", 
-        meaning: "Потеря, сожаление, разочарование", 
-        image: "images/cups05.jpg",
-        type: "cups"
-    },
-    { 
-        name: "6 Кубков", 
-        meaning: "Ностальгия, невинность, детство", 
-        image: "images/cups06.jpg",
-        type: "cups"
-    },
-    { 
-        name: "7 Кубков", 
-        meaning: "Выбор, иллюзии, мечты", 
-        image: "images/cups07.jpg",
-        type: "cups"
-    },
-    { 
-        name: "8 Кубков", 
-        meaning: "Уход, поиск, отказ", 
-        image: "images/cups08.jpg",
-        type: "cups"
-    },
-    { 
-        name: "9 Кубков", 
-        meaning: "Удовлетворение, комфорт, удовольствие", 
-        image: "images/cups09.jpg",
-        type: "cups"
-    },
-    { 
-        name: "10 Кубков", 
-        meaning: "Счастье, семья, гармония", 
-        image: "images/cups10.jpg",
-        type: "cups"
-    },
-    { 
-        name: "Паж Кубков", 
-        meaning: "Мечтательность, чувствительность, творчество", 
-        image: "images/cups11.jpg",
-        type: "cups"
-    },
-    { 
-        name: "Рыцарь Кубков", 
-        meaning: "Романтика, очарование, идеализм", 
-        image: "images/cups12.jpg",
-        type: "cups"
-    },
-    { 
-        name: "Королева Кубков", 
-        meaning: "Забота, интуиция, сострадание", 
-        image: "images/cups13.jpg",
-        type: "cups"
-    },
-    { 
-        name: "Король Кубков", 
-        meaning: "Эмоциональный контроль, дипломатия", 
-        image: "images/cups14.jpg",
-        type: "cups"
-    },
-
-    // МЛАДШИЕ АРКАНЫ - МЕЧИ (14 карт)
-    { 
-        name: "Туз Мечей", 
-        meaning: "Прорыв, ясность, истина", 
-        image: "images/swords01.jpg",
-        type: "swords"
-    },
-    { 
-        name: "2 Мечей", 
-        meaning: "Тупик, баланс, решение", 
-        image: "images/swords02.jpg",
-        type: "swords"
-    },
-    { 
-        name: "3 Мечей", 
-        meaning: "Сердечная боль, печаль, конфликт", 
-        image: "images/swords03.jpg",
-        type: "swords"
-    },
-    { 
-        name: "4 Мечей", 
-        meaning: "Отдых, восстановление, медитация", 
-        image: "images/swords04.jpg",
-        type: "swords"
-    },
-    { 
-        name: "5 Мечей", 
-        meaning: "Конфликт, унижение, поражение", 
-        image: "images/swords05.jpg",
-        type: "swords"
-    },
-    { 
-        name: "6 Мечей", 
-        meaning: "Переход, движение, улучшение", 
-        image: "images/swords06.jpg",
-        type: "swords"
-    },
-    { 
-        name: "7 Мечей", 
-        meaning: "Обман, стратегия, секреты", 
-        image: "images/swords07.jpg",
-        type: "swords"
-    },
-    { 
-        name: "8 Мечей", 
-        meaning: "Ограничения, страх, беспомощность", 
-        image: "images/swords08.jpg",
-        type: "swords"
-    },
-    { 
-        name: "9 Мечей", 
-        meaning: "Тревога, кошмары, депрессия", 
-        image: "images/swords09.jpg",
-        type: "swords"
-    },
-    { 
-        name: "10 Мечей", 
-        meaning: "Конец, предательство, поражение", 
-        image: "images/swords10.jpg",
-        type: "swords"
-    },
-    { 
-        name: "Паж Мечей", 
-        meaning: "Любознательность, остроумие, бдительность", 
-        image: "images/swords11.jpg",
-        type: "swords"
-    },
-    { 
-        name: "Рыцарь Мечей", 
-        meaning: "Амбиции, скорость, прямота", 
-        image: "images/swords12.jpg",
-        type: "swords"
-    },
-    { 
-        name: "Королева Мечей", 
-        meaning: "Ясность, независимость, принципы", 
-        image: "images/swords13.jpg",
-        type: "swords"
-    },
-    { 
-        name: "Король Мечей", 
-        meaning: "Интеллект, власть, истина", 
-        image: "images/swords14.jpg",
-        type: "swords"
-    },
-
-    // МЛАДШИЕ АРКАНЫ - ПЕНТАКЛИ (14 карт)
-    { 
-        name: "Туз Пентаклей", 
-        meaning: "Процветание, возможность, изобилие", 
-        image: "images/pents01.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "2 Пентаклей", 
-        meaning: "Баланс, адаптация, приоритеты", 
-        image: "images/pents02.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "3 Пентаклей", 
-        meaning: "Мастерство, сотрудничество, ремесло", 
-        image: "images/pents03.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "4 Пентаклей", 
-        meaning: "Сохранение, контроль, безопасность", 
-        image: "images/pents04.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "5 Пентаклей", 
-        meaning: "Нужда, беспокойство, изоляция", 
-        image: "images/pents05.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "6 Пентаклей", 
-        meaning: "Щедрость, благотворительность, обмен", 
-        image: "images/pents06.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "7 Пентаклей", 
-        meaning: "Терпение, оценка, перспективы", 
-        image: "images/pents07.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "8 Пентаклей", 
-        meaning: "Мастерство, качество, ремесло", 
-        image: "images/pents08.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "9 Пентаклей", 
-        meaning: "Изобилие, роскошь, самообеспечение", 
-        image: "images/pents09.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "10 Пентаклей", 
-        meaning: "Богатство, наследие, семья", 
-        image: "images/pents10.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "Паж Пентаклей", 
-        meaning: "Изучение, усердие, потенциал", 
-        image: "images/pents11.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "Рыцарь Пентаклей", 
-        meaning: "Трудолюбие, надежность, ответственность", 
-        image: "images/pents12.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "Королева Пентаклей", 
-        meaning: "Изобилие, комфорт, безопасность", 
-        image: "images/pents13.jpg",
-        type: "pentacles"
-    },
-    { 
-        name: "Король Пентаклей", 
-        meaning: "Процветание, бизнес, лидерство", 
-        image: "images/pents14.jpg",
-        type: "pentacles"
-    }
-];
-
 class TarotApp {
     constructor() {
         this.selectedCards = [];
@@ -525,35 +44,45 @@ class TarotApp {
     }
 
     updateLayout() {
-        // Добавляем классы для разных размеров экрана
         document.body.className = '';
         document.body.classList.add(`screen-${this.screenSize}`);
         if (this.isMobile) {
             document.body.classList.add('mobile');
         }
         
-        // Обновляем отображение карт при изменении layout
         this.renderCards();
     }
 
     preloadAssets() {
-        // Предзагрузка фона приложения
+        // Улучшенная загрузка фона
+        this.loadBackground();
+        this.loadCardBack();
+    }
+
+    loadBackground() {
         const bgImage = new Image();
         bgImage.src = 'images/background.jpg';
         
         bgImage.onload = () => {
             console.log('✅ Фон приложения загружен');
             this.backgroundLoaded = true;
-            document.body.style.background = 'url("images/background.jpg") center/cover fixed';
+            // Принудительно устанавливаем фон
+            document.body.style.backgroundImage = 'url("images/background.jpg"), linear-gradient(135deg, #667eea 0%, #764ba2 100%)';
+            document.body.style.backgroundSize = 'cover, auto';
+            document.body.style.backgroundPosition = 'center, center';
+            document.body.style.backgroundRepeat = 'no-repeat, no-repeat';
+            document.body.style.backgroundAttachment = 'fixed, fixed';
         };
         
         bgImage.onerror = () => {
             console.warn('❌ Фон приложения не загружен, используем градиент');
             this.backgroundLoaded = false;
-            document.body.style.background = 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)';
+            document.body.style.backgroundImage = 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)';
+            document.body.style.backgroundSize = 'auto';
         };
+    }
 
-        // Предзагрузка рубашки карт
+    loadCardBack() {
         const cardBackImage = new Image();
         cardBackImage.src = 'images/card_back.jpg';
         
@@ -578,7 +107,10 @@ class TarotApp {
                 back.style.background = 'linear-gradient(45deg, #8B4513, #A0522D)';
             } else {
                 back.classList.remove('fallback');
+                // Принудительно растягиваем изображение на всю площадь
                 back.style.background = 'url("images/card_back.jpg") center/cover no-repeat';
+                back.style.backgroundSize = 'cover';
+                back.style.backgroundPosition = 'center';
             }
         });
     }
@@ -627,7 +159,7 @@ class TarotApp {
                     <div class="card-back ${cardBackClass}"></div>
                     <div class="card-front">
                         <img src="${card.image}" alt="${card.name}" class="card-image"
-                             onerror="this.style.display='none';">
+                             onerror="this.style.display='none'; this.nextElementSibling.style.display='flex'; this.nextElementSibling.style.alignItems='center'; this.nextElementSibling.style.justifyContent='center'; this.nextElementSibling.style.height='100%';">
                         <div class="card-info">
                             <div class="card-name">${this.getShortName(card.name)}</div>
                             <div class="card-meaning">${card.meaning}</div>
@@ -639,6 +171,11 @@ class TarotApp {
             cardElement.addEventListener('click', () => this.toggleCard(card, index));
             container.appendChild(cardElement);
         });
+        
+        // Обновляем рубашки после рендера
+        setTimeout(() => {
+            this.updateCardBacks();
+        }, 100);
         
         this.updateResults();
     }
@@ -737,12 +274,10 @@ class TarotApp {
             shuffleBtn.addEventListener('click', () => this.shuffleCards());
         }
 
-        // Обработчик изменения размера окна
         window.addEventListener('resize', () => {
             this.handleResize();
         });
 
-        // Предотвращаем масштабирование при двойном тапе
         document.addEventListener('touchstart', (e) => {
             if (e.touches.length > 1) {
                 e.preventDefault();
@@ -759,7 +294,6 @@ class TarotApp {
             this.updateLayout();
         }
         
-        // Адаптация для ландшафтного режима
         if (window.innerHeight < 500) {
             document.body.classList.add('landscape');
         } else {
@@ -830,11 +364,23 @@ class TarotApp {
             });
         }, 300);
     }
+
+    // Метод для принудительной перезагрузки фона
+    reloadBackground() {
+        this.loadBackground();
+    }
 }
 
 // Запуск приложения
 document.addEventListener('DOMContentLoaded', () => {
     window.tarotApp = new TarotApp();
+    
+    // Добавляем глобальную функцию для перезагрузки фона
+    window.reloadBackground = function() {
+        if (window.tarotApp) {
+            window.tarotApp.reloadBackground();
+        }
+    };
     
     window.addEventListener('error', (e) => {
         if (e.target.tagName === 'IMG') {
